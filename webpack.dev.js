a4299308caac93e84b6384f35c0cca3380a1b82f
Erick Christian <erickchristian48@gmail.com>
2020-08-09 22:35:17 +0700

    Created

---
+++ webpack.dev.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+/** @type {import ('webpack').Configuration} */
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  devServer: {
+    contentBase: '/dist',
+  },
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+  },
+
+  entry: './src/scripts/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts(x)?$/,
+        use: ['ts-loader'],
+      },
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpg|jpeg|png)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/html/index.html' })],
+}
